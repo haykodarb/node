9caa583667e3185e55527713ec447b920eef120d
Hayk <haykodarb@gmail.com>
2020-04-16 17:10:19 -0300

    Cambio de solicitud GET a POST

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ var port = process.env.PORT || '3306';
 app.listen(port, () => { console.log(`Server started on port ${port}`)
 } );
 
-var estados = {
-  setPoint: 0,
-  estadoActual: 0,
-  estadoApp: 0 
-} 
 
-var con = mysql.createConnection({
+var con = mysql.createConnection(
+  {
   host: "us-cdbr-iron-east-01.cleardb.net",
   user: "b93244dc053f45",
   password: "937a4217",
@@ -31,7 +27,7 @@ con.connect( (err) => {
     } } else {
     console.log('Conexion correcta'); }
   });
-
+  
 app.get('/select', (req, res) => {
     let sql = `SELECT setPoint, estadoActual, estadoApp FROM estados ORDER BY ID DESC LIMIT 1`; 
     con.query(sql, (err, result) => {
@@ -40,32 +36,43 @@ app.get('/select', (req, res) => {
           throw err;
         } catch (e) {
           console.log('Sucedio un error al recibir: ', e);
+          res.end();
         }
       } else {
         console.log("Select hecho correctamente correctamente");
-        estados = result[0];
+        var estados = result[0];
         estados = JSON.stringify(estados);
-        res.send(estados);
+        res.send(estados); // No hace falta .send, .end también envía.
         res.end();
       }  
     });
 });
     
-app.get('/insert', (req, res) =>{
-  let datos = req.query;
+app.post('/insert', (req, res) =>{
+  let datos = req.body;
+  if (!datos.temp) {
+    res.end("Parametros vacios o incorrectamente enviados");
+    return;
+    } 
   let temp = datos.temp; 
   let serie = datos.serie;
   let sql = `INSERT INTO datos (id, serie, fecha, temp) VALUES (NULL, ${serie}, CURRENT_TIMESTAMP, ${temp});`;
   con.query(sql, function (err) {
     if (err) {
       try {
-        throw err;
-      } catch (e) {
-        console.log('Sucedio un error al recibir: ', e);
-      }
+        throw err;}
+      catch (e) {
+        console.log('Sucedio un error al recibir: ', e);}
+        res.end('Sucedio un error al recibir: ', e);
     } else { 
-      res.send("");
-      res.end();
-    } 
+      res.end(`Recibido correctamente: ${datos}`);} 
     });
 });
+
+app.get('/sumar', (req,res) => {
+  res.end("HOLA!!!!")
+})
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+})
