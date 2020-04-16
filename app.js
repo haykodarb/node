10877e51edad5ee5b0515a0fc34d5eed6ea7cd4f
Hayk <haykodarb@gmail.com>
2020-04-16 18:07:15 -0300

    Cambiado de e a err

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const http = require('http');
 const url = require('url');
 const app = express();
-const bodyParser = requier('body-parser'); 
+const bodyParser = require('body-parser'); 
 
 var port = process.env.PORT || '3306';
 
@@ -50,22 +50,18 @@ app.get('/select', (req, res) => {
 });
     
 app.post('/insert', (req, res) =>{
-  let data = {
+  var data = {
     temp: req.body.temp,
     serie: req.body.serie
   };
-  if (!data.temp) {
-    res.end("Parametros vacios o incorrectamente enviados");
-    return;
-    } 
   let sql = `INSERT INTO datos (id, serie, fecha, temp) VALUES (NULL, ${data.serie}, CURRENT_TIMESTAMP, ${data.temp});`;
   con.query(sql, function (err) {
     if (err) {
       try {
         throw err;}
-      catch (e) {
-        console.log('Sucedio un error al recibir: ', e);}
-        res.end('Sucedio un error al recibir: ', e);
+      catch (err) {
+        console.log('Sucedio un error al recibir: ', err);}
+        res.end('Sucedio un error al recibir: ', err);
     } else { 
       res.end('Recibido correctamente');} 
     });
