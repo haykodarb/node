7d1e25334adb13ba11a50e70e7b8adf61223259d
Mikael Sargsyan <mikael.sargsyan@aerolab.co>
2020-04-17 22:31:16 -0300

    Adding nodemon && small syntax on app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
-const http = require('http');
+const chalk = require('chalk');
 const app = express();
+const http = require('http').createServer(app);
 const bodyParser = require('body-parser'); 
-
-
-app.listen(app.get('port'), () => console.log(`Server started on port ${app.get('port')}`));
 
 app.use(express.json());
 
-var con = mysql.createConnection(
-  {
+http.listen(3000, () => {
+  console.log(chalk.green('Listening on port: 3000'));
+});
+
+let con = mysql.createConnection({
   host: "us-cdbr-iron-east-01.cleardb.net",
   user: "b93244dc053f45",
   password: "937a4217",
@@ -23,48 +24,48 @@ con.connect((err) => {
       throw err;
     } catch (e) {
       console.log(`Fallo al conectarse a la base de datos: ${e}`);
+      return;
     }
-    
   } else {
     console.log('Conexion correcta');
   }
 });
 
 app.get('/select', (req, res) => {
-    let sql = `SELECT setPoint, estadoActual, estadoApp FROM estados ORDER BY ID DESC LIMIT 1`; 
+    const sql = `SELECT setPoint, estadoActual, estadoApp FROM estados ORDER BY ID DESC LIMIT 1`; 
+
     con.query(sql, (err, result) => {
-              if (err) {
-          try {
-            throw err;
-          } catch (e) {
-            res.status(400).json({ errorMessage: `Endpoint: ${req.path}. Sucedio un error al recibir: ${e}` });
-          }
-        } else {
-          estados = result[0];
-          // Mas info aca http://expressjs.com/es/api.html#res.json
-          res.status(200).json(estados);
-        }      
+      if (err) {
+        try {
+          throw err;
+        } catch (e) {
+          res.status(400).json({ errorMessage: `Endpoint: ${req.path}. Sucedio un error: ${e}` });
+        }
+      } else {
+        // Mas info aca http://expressjs.com/es/api.html#res.json
+        res.status(200).json(result[0]);
+      }      
     });
 });
     
 app.post('/insert', (req, res) =>{
-  var data = {
+  const data = {
     temp: req.body.temp,
     serie: req.body.serie
   };
-  let sql = `INSERT INTO datos (id, serie, fecha, temp) VALUES (NULL, ${data.serie}, CURRENT_TIMESTAMP, ${data.temp});`;
+  const sql = `INSERT INTO datos (id, serie, fecha, temp) VALUES (NULL, ${data.serie}, CURRENT_TIMESTAMP, ${data.temp});`;
+
   con.query(sql, (err) => {
     if (err) {
       try {
-        throw err;}
-      catch (e) {
+        throw err;
+      } catch (e) {
         res.status(400).json({ errorMessage: `Endpoint: ${req.path}. Sucedio un error al recibir: ${e}`});
-            }
-     }
-      else {
-        var jason = JSON.stringify(data);
-        res.status(200);} 
-    });
+      }
+    } else {
+      res.status(200);
+    }
+  });
 });
 
 module.exports = app;
