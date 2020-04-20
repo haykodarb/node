daddc68ab25e08cff84fb7aa82baa3dd95a0196e
Hayk <haykodarb@gmail.com>
2020-04-20 10:13:04 -0300

    Agregados estados.json y datos.json

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 const app = express();
 const http = require('http').createServer(app);
 const bodyParser = require('body-parser'); 
+const fs = require('fs');
 
 app.use(express.json());
 
@@ -20,38 +21,31 @@ let con = mysql.createPool({
 });
 
 app.get('/select', (req, res) => {
-  const sql = `SELECT estadoApp, estadoActual, setPoint FROM estados ORDER BY ID DESC LIMIT 1`; 
-
-  con.query(sql, (err, result) => {
-    if (err) {
-      try {
-        throw err;
-      } catch (e) {
-          res.status(400).json({ errorMessage: `Endpoint: ${req.path}. Sucedio un error: ${e}` });
-        }
-    } else {
-      res.status(200).json(result[0]);
-      }      
-    });
+  let rawdata = fs.readFileSync('estados.json');
+  let estados = JSON.parse(rawdata);
+  res.status(200).json(estados);
 });
     
 app.post('/insert', (req, res) =>{
-  const data = {
-    temp: req.body.temp,
-    serie: req.body.serie
-  };
+  let datos = req.body;
+  fs.writeFileSync('datos.json', datos);
+  res.status(200).send('Post realizado correcctamente');
+  insertSQL(datos);
+});
+
+function insertSQL(data) {
   const sql = `INSERT INTO datos (id, serie, temp, hora) VALUES (NULL, ${data.serie}, ${data.temp}, CURRENT_TIMESTAMP)`;
   con.query(sql, (err) => {
     if (err) {
       try {
         throw err;
       } catch (e) {
-        res.status(400).json({ errorMessage: `Endpoint: ${req.path}. Sucedio un error al recibir: ${e}`});
+        console.log(`errorMessage: Sucedio un error al recibir: ${e}`);
       }
     } else {
-      res.status(200).send("Post hecho correctamente");
+      console.log('Insert realizado correctamente');
     }
   });
-});
+}
 
 module.exports = app;
