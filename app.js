99e68eb40b66fb239868c893dde704f6d3aef88f
Hayk <haykodarb@gmail.com>
2020-05-09 16:22:52 -0300

    Probando devuelta

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.get('/graficos/:id', (req, res) =>  {
         dataArray.tempArray[i] = result[i].temp;
         dataArray.humArray[i] = result[i].hum; 
         dataArray.lumArray[i] = result[i].lum;
-        dataArray.timeArray[i] = moment(`${result[i].time}`, "DD-MM-YYYY HH:mm:ss");
+        dataArray.timeArray[i] = moment(result[i].time, "DD-MM-YYYY HH:mm:ss");
       }
       res.status(200).json(dataArray);
       }
