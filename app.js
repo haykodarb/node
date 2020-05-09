aa9e7521c876dfe979c4eebb6b3be9155a5f85c4
Hayk <haykodarb@gmail.com>
2020-05-09 15:29:28 -0300

    Cambiando formato de las fechas y tiempos

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ http.listen(3000, () => {
 
 
 function obtenerFecha() {
-  let now = moment().tz('America/Argentina/Buenos_Aires').format();
+  let now = moment().tz('America/Argentina/Buenos_Aires').format('YYYY:MM:DD HH:mm:ss');
   return now;
 }
 
