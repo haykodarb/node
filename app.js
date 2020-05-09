925a2ff719d54543b3bdb37c85e877cec7d1c087
Hayk <haykodarb@gmail.com>
2020-05-09 15:31:20 -0300

    Estaba mal el formato, corrijo denuevo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ http.listen(3000, () => {
 
 
 function obtenerFecha() {
-  let now = moment().tz('America/Argentina/Buenos_Aires').format('YYYY:MM:DD HH:mm:ss');
+  let now = moment().tz('America/Argentina/Buenos_Aires').format('YYYY-MM-DD HH:mm:ss');
   return now;
 }
 
