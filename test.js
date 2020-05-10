1939c3e5457bcaf6aaaeea9aaaf728d90b83a6df
Hayk <haykodarb@gmail.com>
2020-05-10 14:40:41 -0300

    Deleted unnecesary files

--- test.js
+++
@@ -1,5 +0,0 @@
- let moment = require('moment-timezone');
-
- let now = moment().tz('America/Argentina/Buenos_Aires').subtract(0, 'days').hour(0).minute(0).second(0).format('YYYY-MM-DD HH:mm:ss');
- 
- console.log(now);
