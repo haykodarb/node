c835bbd3c1580f0a587adcbdd3d713e539784a9f
Hayk <haykodarb@gmail.com>
2020-05-12 18:04:52 -0300

    Fixing error on incorrect password login

--- fileserver.js
+++ fileserver.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-
-
 router.get('/views/login.ejs', (req, res) => {
     res.sendFile(__dirname + "/views/login.ejs"); 
 }); 
