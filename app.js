c961ab5e49e7e135c62222095f54fdd4ba5263eb
Hayk <haykodarb@gmail.com>
2020-05-12 13:57:44 -0300

    No estoy seguro cual fue el ultimo cambio

--- app.js
+++ app.js
@@ -34,9 +34,12 @@ app.get('/', verify, (req, res) => {
 app.get('/views/login.ejs', (req, res) => {
     res.sendFile(__dirname + "/views/login.ejs"); 
 }); 
+
+
 app.get('/views/register.ejs', (req, res) => {
     res.sendFile(__dirname + "/views/register.ejs"); 
 }); 
+
 app.get('/views/dashboard.ejs', (req, res) => {
     res.sendFile(__dirname + "/views/dashboard.ejs"); 
 }); 
