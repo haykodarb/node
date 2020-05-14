1edbc501ac90a4ea3a614b5e45bfd13de37ff517
Hayk <haykodarb@gmail.com>
2020-05-14 20:58:17 -0300

    Last changes were unnecessary

--- routes/api.js
+++ routes/api.js
@@ -66,6 +66,8 @@ router.post('/graficos', (req, res) => {
             let dataArray = {
                 tempArray: [],
                 humArray: [],
+                lumArray: [],
+                timeArray: [],
             };
             for (let i = 0; i < result.length; i++) {
                 dataArray.tempArray[i] = result[i].temp;
