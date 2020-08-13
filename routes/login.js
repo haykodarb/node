a1816166a8ac43965ae4473f44c94f41a22c1362
haykodarb <haykodarb@gmail.com>
2020-08-13 12:39:03 -0300

    Console logging to check cookies working

--- routes/login.js
+++ routes/login.js
@@ -79,6 +79,7 @@ router.post('/', (req, res) => {
                             expires: farFuture,
                             secure: true,
                         });
+                        console.log("User has token", token);
                         res.redirect('../dashboard');
                     }
                 } else {
