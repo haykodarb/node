825385030e9882fb28a6e9aec3d4b8a64e0e3f66
haykodarb <haykodarb@gmail.com>
2020-08-13 12:57:35 -0300

    Changing verify method, maybe this works

--- tools/tokenVerify.js
+++ tools/tokenVerify.js
@@ -1,21 +1,25 @@
-const jwt = require('jsonwebtoken');
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const router = express.Router();
 
 router.use(cookieParser());
 
 module.exports = function verify(req, res, next) {
-    if (typeof req.cookies === 'undefined') {
-        return res.redirect('./login');
-    } else {
-        try {
-            const token = req.cookies.token;
-            let data = jwt.verify(token, process.env.token_secret);
-            next();
-        } catch {
-            return res.redirect('./login');
-        }
-    }
+  if (typeof req.cookies === "undefined") {
+    return res.redirect("./login");
+  } else {
+    const token = req.cookies.token;
+    jwt.verify(token, process.env.token_secret, function (
+      err,
+      decoded
+    ) {
+      if (typeof decoded === 'undefined') {
+        res.redirect("./login");        
+      } else {
+        next();
+      }
+    });
+  }
 };
