34064f6cd9e1f651fb49fcee4892e25db962e1aa
haykodarb <haykodarb@gmail.com>
2021-05-16 19:28:26 -0300

    Added endpoint only for testing

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
 //Modules
-const dotenv = require('dotenv').config();
-const express = require('express');
+const dotenv = require("dotenv").config();
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const cors = require('cors');
-const chalk = require('chalk');
-const http = require('http').createServer(app);
+const ejs = require("ejs");
+const cors = require("cors");
+const chalk = require("chalk");
+const http = require("http").createServer(app);
 
 let port = process.env.PORT || 3000;
 
 //Server config
 http.listen(port, () => {
-    console.log(chalk.green(`Listening on port: ${port}`));
+  console.log(chalk.green(`Listening on port: ${port}`));
 });
 
 //Routes
-const api = require('./routes/api');
-const dashboard = require('./routes/dashboard');
-const login = require('./routes/login');
-const register = require('./routes/register');
-const verify = require('./tools/tokenVerify');
+const api = require("./routes/api");
+const dashboard = require("./routes/dashboard");
+const login = require("./routes/login");
+const register = require("./routes/register");
+const verify = require("./tools/tokenVerify");
 
 //Middleware
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
-app.use('/api', api);
-app.use('/dashboard', dashboard);
-app.use('/login', login);
-app.use('/register', register);
+app.use(express.static(__dirname + "/public"));
+app.use("/api", api);
+app.use("/dashboard", dashboard);
+app.use("/login", login);
+app.use("/register", register);
 
 //Views config
-app.set('views', './public/views');
-app.set('view engine', 'ejs');
+app.set("views", "./public/views");
+app.set("view engine", "ejs");
 
-app.get('/', verify, (req, res) => {
-    res.redirect('./dashboard');
+app.get("/", verify, (req, res) => {
+  res.redirect("./dashboard");
+});
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("PING");
 });
